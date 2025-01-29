5f265637dc802ae286fbf421e1df20da31b527e1
Dothrakii <jatinyadav282828@gmail.com>
2025-01-29 17:43:55 +0000

    Conversion of Datatypes

--- 01_starter/test.js
+++ 01_starter/test.js
@@ -1,1 +1,10 @@
 console.log("Jatin")
+let score = "33abc"
+let convertinNumber = Number(score)
+
+console.log(typeof score)
+console.log(convertinNumber)
+
+let isBoolean = 4
+let ConvertisBoolean = Boolean(isBoolean)
+console.log(ConvertisBoolean)
